815f54d0cb94e91e0fb3d5b71414f2fde2eec0dd
Jasonzhi123 <1244653559@qq.com>
2018-10-01 17:41:03 +0800

    15

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import {
-  message,
-  Button
-} from 'antd';
-
-const info = () => {
-  message.info('This is a normal message');
-};
-
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Button type="primary"
-          onClick={
-            info
-          } > Display normal message </Button>,
-        );
+      <div>
+       {this.props.children}
       </div>
     );
   }
